514330c6439bf9e5cff570a4f415b413e8d584db
Zainab Siddiqui <107427506+Zainab345@users.noreply.github.com>
2023-01-06 22:43:05 -1100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function show() {
+  document.getElementById('sidebar').classList.toggle('action');
+}
+
+function cross() {
+  document.getElementById('sidebar').classList.toggle('action');
+}
+
+function showmob() {
+  document.getElementById('sidebarmob').classList.toggle('actionmob');
+}
+
+function crossmob() {
+  document.getElementById('sidebarmob').classList.toggle('actionmob');
+}
+
